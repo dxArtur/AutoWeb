0293aef11b7d832dbb843110c5391258ad6969fd
Israel <israeloliveira171@gmail.com>
2024-03-25 13:15:11 -0300

    Fix drive postgresql

--- src/main/java/com/AutoWeb/database/ConnectionFactory.java
+++ src/main/java/com/AutoWeb/database/ConnectionFactory.java
@@ -5,17 +5,21 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-	private static final String URL = "";
-    private static final String USER = "";
-    private static final String PASSWORD = "";
+	private static final String URL = "jdbc:postgresql://localhost:5432/autoweb";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+
 	
-	public Connection getConnection()  {
-		try {
-			System.out.println("conecting with database");
-			return DriverManager.getConnection(URL, USER, PASSWORD);
-		}catch (SQLException e) {
-			throw new RuntimeException(e);
-		}	
-	}
+    public Connection getConnection() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            System.out.println("Conectando ao banco de dados");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao conectar ao banco de dados: " + e.getMessage(), e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Driver do PostgreSQL não encontrado no classpath", e);
+        }
+    }
 
 }
