2449838aad404634dfc9b6189d0bb91888f57522
Israel <israeloliveira171@gmail.com>
2024-04-01 12:42:54 -0300

    BD & Fixes

--- src/main/java/com/AutoWeb/database/ConnectionFactory.java
+++ src/main/java/com/AutoWeb/database/ConnectionFactory.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
 
-	private static final String URL = "jdbc:postgresql://localhost:5432/autoweb";
+	private static final String URL = "jdbc:postgresql://localhost:5432/autowebdac";
     private static final String USER = "postgres";
     private static final String PASSWORD = "postgres";
 
